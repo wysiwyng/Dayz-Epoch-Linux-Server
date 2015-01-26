6e77dac77e4768b95a15a450f47d0c9147897c4c
wysiwyng <wysiwyng@users.noreply.github.com>
2015-01-26 10:13:54 +0100

    added auto update feature

--- restarter.pl
+++ restarter.pl
@@ -6,14 +6,28 @@
 
 use warnings;
 use strict;
+use File::Copy;
 
 use constant PORT      => 2302; # Change it with epoch.sh
-use constant PATH      => $ENV{'PWD'}.'/'; # Set your epoch server dir
+use constant PATH      => '/home/gameserver/epoch/'; # Set your epoch server dir
 use constant PIDFILE   => PATH.PORT.'.pid';
 use constant CACHE_DIR => PATH.'cache/players';
+use constant UPDATE_DIR => PATH.'update';
+
+# files to be auto updated on server restart, format: <file name> => PATH."original file path"
+my %files_to_update = (
+    "writer.pl"                     => PATH."writer.pl",
+    "dayz_epoch_11.chernarus.pbo"   => PATH."mpmissions/dayz_epoch_11.chernarus.pbo",
+    "createvehicle.txt"             => PATH."expansion/battleye/createvehicle.txt",
+    "dayz_server.pbo"               => PATH."\@dayz_epoch_server/addons/dayz_server.pbo",
+    "server.cfg"                    => PATH."cfgdayz/server.cfg",
+    "epoch.sh"                      => PATH."epoch.sh",
+    "epoch"                         => PATH."epoch",
+    "cfgdayz.arma2oaprofile"        => PATH."/cfgdayz/cfgdayz.arma2oaprofile",
+);
 
 unless (-f PATH.'epoch') {
-    print STDERR "Can't found server binary!\n";
+    print STDERR "Can't find server binary!\n";
     exit;
 }
 
@@ -32,6 +46,8 @@ if (-f PIDFILE) {
     backup_cache();
 }
 
+update_files ();
+
 print STDERR "Restart Dayz Epoch server...\n";
 chdir (PATH);
 
@@ -41,11 +57,38 @@ print STDERR $res,"\n" if $res;
 exit;
 
 #-----------------------------------------------------------------------------------------------
+sub update_files {
+    my ($s, $m, $h, $day, $mon, $y) = localtime(time());
+    $y += 1900;
+    $mon++;
+
+    return unless (-d UPDATE_DIR);
+    
+    opendir (DIR, UPDATE_DIR) or die "Cant't open: $!";
+    my @files = readdir (DIR);
+    closedir (DIR);
+    
+    foreach my $file (@files) {
+        next if (not exists $files_to_update{$file});
+        
+        my $file_to_update = $files_to_update{$file};
+        
+        if (-f $file_to_update) {
+            print STDERR "file $file exists, backing up\n";
+            my $new_name = sprintf("%s\.%02d%02d%d-%02d-%02d", $file_to_update, $day, $mon, $y, $h, $m);
+            print STDERR "new name: $new_name\n";
+            move ($file_to_update, $new_name) or die "Can't backup file $file_to_update!";
+        }
+        
+        move (UPDATE_DIR."/".$file, $file_to_update) or die "Can't install new file $file_to_update!";
+    }    
+}
+
 sub set_time {
     my ($s, $m, $h, $day, $mon, $y) = localtime(time() - 3*3600);
     $y += 1900;
     $mon++;
-    
+
     # Uncomment to disabe night
     #($h, $m) = (17, 0) if ($h > 17 || ($h >= 0 && $h < 4));
     
